1e0ec82fcb77cf5360e597133627abf35a3835e7
TikoTheArmenian <bdoyant23@columbusacademy.org>
2023-01-20 09:14:10 -0500

    Fixed bugs

--- src/Play.java
+++ src/Play.java
@@ -5,8 +5,13 @@ public class Play {
     public static void main(String[] args)
     {
 
+
         ArrayList<Bot> bots = new ArrayList<Bot>();
         bots.add(new Tester2());
+        bots.add(new Tester2());
+        bots.add(new Tester2());
+        bots.add(new Tester2());
+
 
         Display display = new Display(200 , 32, bots,160); // height MUST be divisible by 8 and width must be greater than 1/5 of height
         display.run();
