031ee6f988329e94a4f7fecaa8e1da45a50db659
TikoTheArmenian <bdoyant23@columbusacademy.org>
2023-01-25 12:05:09 -0500

    Fixed bot disapearing

--- src/Play.java
+++ src/Play.java
@@ -6,8 +6,7 @@ public class Play {
     {
 
         ArrayList<Bot> bots = new ArrayList<Bot>();
-        bots.add(new TestBot3());
-        bots.add(new TestBot3());
+        bots.add(new TestBot());
 
         Display display = new Display(200 , 32, bots,120); // height MUST be divisible by 8 and width must be greater than 1/5 of height
         display.run();
